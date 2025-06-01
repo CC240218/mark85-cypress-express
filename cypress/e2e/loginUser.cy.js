2a4c43fd532688660e66fe368a18f3b6f5de0218
Rafael santos <rp1437508@gmail.com>
2025-06-01 17:38:38 -0300

    finalização da suite de teste de login na api

--- cypress/e2e/loginUser.cy.js
+++ cypress/e2e/loginUser.cy.js
@@ -31,12 +31,67 @@ describe('Teste de login via API', () => {
                 expect(response.body.expires_in).to.eq('10d');
 
             cy.wrap(response.body.token).as('token');
-            
+
             });
             
         });
     });
 
-    context('Validation Login', () => {});
+    context('Validation Login', () => {
+
+        it('Não deve logar usuário não cadastrado. Versão email', () => {
+
+            data.users.email = 'teste@emailerr.com';
+            cy.loginUser(data.users, {}).then(response => {
+                expect(response.status).to.eq(401);
+                expect(response.body).to.not.exist;
+            });
+
+        });
+
+        it('Não deve logar usuário não cadastrado. Versão senha', () => {
+
+            data.users.password = 'senhaerrada';
+            cy.loginUser(data.users, {}).then(response => {
+                expect(response.status).to.eq(401);
+                expect(response.body).to.not.exist;
+            });
+
+        });
+
+        it('Nao deve cadastrar com email inválido', () => {
+            data.users.email = 'emailinvalido';
+            cy.loginUser(data.users, {}).then(response => {
+                expect(response.status).to.eq(400);
+                expect(response.body).to.have.property('type');
+                expect(response.body).to.have.property('message');
+                expect(response.body.message).to.eq(data.err.msnInv);
+            });
+
+        });
+
+        it('Não deve cadastrar com email vazio', () => {
+
+            data.users.email = '';
+            cy.loginUser(data.users, {}).then(response => {
+                expect(response.status).to.eq(400);
+                expect(response.body).to.have.property('type');
+                expect(response.body).to.have.property('message');
+                expect(response.body.message).to.eq(data.err.emlEmpy);
+            });
+
+        });
+
+        it('Não deve cadastrar com senha vazia', () => {
+            data.users.password = '';
+            cy.loginUser(data.users, {}).then(response => {
+                expect(response.status).to.eq(400);
+                expect(response.body).to.have.property('type');
+                expect(response.body).to.have.property('message');
+                expect(response.body.message).to.eq(data.err.pwdEmpy);
+            });
+        });
+
+    });
 
 })
