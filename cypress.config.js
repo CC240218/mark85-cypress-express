d49c922cec01398507dc7b1068fbf8abd6522f09
Rafael santos <rp1437508@gmail.com>
2025-06-02 10:27:39 -0300

    comentei o codigo da task createUser do config, já que não vou usar, mas ficará por questões academicas

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,5 @@
 const { deleteUser } = require("./cypress/support/utils");
-const { insertUser } = require("./cypress/support/utils");
+//const { insertUser } = require("./cypress/support/utils");
 
 
 module.exports = {
@@ -11,12 +11,12 @@ module.exports = {
           return await deleteUser(email)
         }
       });
-      on('task', {
+      /*on('task', {
         async insertUsers(user){
           return await insertUser(user)
         }
-      })
-      // implement node event listeners here
+      })*/
+    
     },
   },
 };
