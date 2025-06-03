670a92596a2211ca7cefcc2286959bbfb1d5f34d
Rafael santos <rp1437508@gmail.com>
2025-06-03 17:34:20 -0300

    Implementação do primeiro teste da suite(incompleto)

--- cypress/e2e/deleteTaskUser.cy.js
+++ cypress/e2e/deleteTaskUser.cy.js
@@ -1,14 +1,28 @@
 
+
 describe('Teste de exclusão de tarefa', () => {
 
     let data;
     beforeEach(() => {
         cy.fixture('users.json').then(user => { data = user; })
         .then(() => cy.task('deleteUserByEmail', data.users.email))
+        .then(() => cy.task('deleteAllTasks'))
         .then(() => cy.createUser(data.users))
         .then(() => cy.loginUser(data.users))
         .then(response =>  cy.wrap(response.body.token).as('loginToken'))
+        .then(() => cy.createTask(data.task))
+        .then(response => cy.wrap(response.body._id).as('task_id'))
     })
 
-    it('Deve excluir uma tarefa', () => {})
+    it('Deve excluir uma tarefa', () => {
+
+        cy.get('@task_id').then(taskId => {
+
+            cy.deleteTask(taskId).then(response => { 
+                expect(response.status).to.eq(204)
+             })
+            
+        })
+
+    })
 })
