138eb1d20f0135895e1820fc1f89d54235a76053
Rafael santos <rp1437508@gmail.com>
2025-06-04 14:49:28 -0300

    Finalizado a suite de exclusão de tarefa

--- cypress/e2e/deleteTaskUser.cy.js
+++ cypress/e2e/deleteTaskUser.cy.js
@@ -1,28 +1,63 @@
 
 
+
 describe('Teste de exclusão de tarefa', () => {
 
     let data;
     beforeEach(() => {
         cy.fixture('users.json').then(user => { data = user; })
-        .then(() => cy.task('deleteUserByEmail', data.users.email))
-        .then(() => cy.task('deleteAllTasks'))
-        .then(() => cy.createUser(data.users))
-        .then(() => cy.loginUser(data.users))
-        .then(response =>  cy.wrap(response.body.token).as('loginToken'))
-        .then(() => cy.createTask(data.task))
-        .then(response => cy.wrap(response.body._id).as('task_id'))
+        .then(()=> cy.setup_createTaskForList(data.users, data.task, null))
+        
     })
 
-    it('Deve excluir uma tarefa', () => {
+    context('Delete task', () => {
 
-        cy.get('@task_id').then(taskId => {
+        it('Deve excluir uma tarefa', () => {
 
-            cy.deleteTask(taskId).then(response => { 
-                expect(response.status).to.eq(204)
-             })
-            
-        })
+            cy.get('@task_id').then(taskId => {
 
+                cy.listTaskBy(taskId).then(response => {
+                expect(response.body._id).to.not.be.empty
+                })
+                .then(() =>  cy.deleteTask(taskId).then(response => { 
+                    expect(response.status).to.eq(204)
+                }))
+                .then(() => cy.listTaskBy(taskId).then(response => {
+                    expect(response.status).to.eq(404)
+                }))
+            });
+        });
     })
+
+    context('Validation delete task', () => {
+
+        it('Deve retornar erro ao não encontrar a tarefa para excluir', () => {
+
+            data.task._id = '123456789123456789123456'; // 24 caracteres
+            cy.deleteTask(data.task._id).then(response => {
+                expect(response.status).to.eq(404);
+                expect(response.body).to.have.lengthOf(0)
+            });
+        });
+
+        it('Deve retornar erro com id abaixo de 24 caracteres', () => {
+
+            data.task._id = '12345678912345678912345'; // 23 caracteres
+            cy.deleteTask(data.task._id).then(response => {
+                expect(response.status).to.eq(400);
+                expect(response.body).to.have.property('message');
+                expect(response.body.message).to.eq(data.err.paranDown)
+            });
+        });
+
+        it('Deve retornar erro com id acima de 24 caracteres', () => {
+
+            data.task._id = '1234567891234567891234567'; // 25 caracteres
+            cy.deleteTask(data.task._id).then(response => {
+                expect(response.status).to.eq(400);
+                expect(response.body).to.have.property('message');
+                expect(response.body.message).to.eq(data.err.paranUp)
+            });
+        });
+    });
 })
